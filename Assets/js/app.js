1b4248af74e8945cdacbe4084d8e785e4b8d008b
unknown <ealdunn99@gmail.com>
2020-06-08 23:09:28 +0400

    slide

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,13 +1,16 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel(
-      {
-        loop:true,
-        autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true
-      }
-    );
-    
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel(
+    {
+      nav: true,
+  navText: [`<i class="fas fa-chevron-double-left"></i>`,`<i class="fas fa-chevron-double-right"></i>`],
+      dots: false,
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true
+    }
+  );
 
 
 
@@ -25,5 +28,6 @@ $(document).ready(function(){
 
 
 
-    
-  });
+
+
+});
