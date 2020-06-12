36236229dd2b3f8939d0186e8ae965cd51b765a0
unknown <ealdunn99@gmail.com>
2020-06-12 12:42:16 +0400

    Expert

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel(
+  $("#owl-carousel").owlCarousel(
     {
       nav: true,
       navText: [`<i class="fas fa-chevron-double-left"></i>`, `<i class="fas fa-chevron-double-right"></i>`],
@@ -12,6 +12,17 @@ $(document).ready(function () {
     }
   );
 
-
-  
+  $("#owl-carousel2").owlCarousel(
+    {
+      nav: true,
+      navText: [`<i class="fas fa-chevron-double-left"></i>`, `<i class="fas fa-chevron-double-right"></i>`],
+      dots: false,
+      items: 3,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true
+    }
+  );
+  $(".num").counterUp({delay:30,time:2000});
 });
